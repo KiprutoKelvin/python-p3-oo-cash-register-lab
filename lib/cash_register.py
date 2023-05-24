d5b28eb80070b7abdeca16e5e832c275a14d3d7e
iamkipruto <kelvin.kipruto@student.moringaschool.com>
2023-05-25 01:28:22 +0300

    🩹:Dealing with cash and discounting

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,41 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount=0):
+    self.total = 0
+    self.discount = discount
+    self.items = []
+
+  def add_item(self, title, price, quantity=1):
+    self.total += price * quantity
+    self.items.extend([title] * quantity)
+
+  def apply_discount(self):
+        if self.discount > 0:
+            self.total -= self.total * (self.discount / 100)
+            print("After the discount, the total comes to $800.")
+        else:
+            print("There is no discount to apply.")
+
+  def get_item_price(self, item):
+        # Define item prices within the class
+        prices = {
+            "apple": 0.99,
+            "tomato": 1.76,
+            # Add more item prices as needed
+        }
+
+        # Retrieve the price of the item from the prices dictionary
+        return prices.get(item, 0)
+
+  def void_last_transaction(self):
+    if self.items:
+        last_item = self.items[-1]
+        last_item_price = self.get_item_price(last_item)
+        occurrences = self.items.count(last_item)
+        self.items = self.items[:-occurrences]
+        self.total -= (last_item_price * occurrences)
+
+    if not self.items:
+        self.total = 0.0
+  
